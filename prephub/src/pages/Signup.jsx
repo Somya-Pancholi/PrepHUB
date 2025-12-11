63fc427399146284efb9b3262219474075f581dd
Somya-Pancholi <asomiy100@gmail.com>
2025-12-11 15:42:02 +0530

    Update API URL

--- prephub/src/pages/Signup.jsx
+++ prephub/src/pages/Signup.jsx
@@ -21,7 +21,7 @@ export default function Signup() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/signup", formData);
+      const res = await axios.post("https://prephub-9sgk.onrender.com/api/auth/signup", formData);
 
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("user", JSON.stringify(res.data.user));
